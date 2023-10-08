66446af28dc81289983a2df4f1d4f2f2c050e763
Viivi Kauppinen <hellothaovy@yahoo.com.vn>
2023-10-08 03:03:29 +0300

    connect flask and react

--- app/server/api.py
+++ app/server/api.py
@@ -1,12 +1,24 @@
 from flask import Flask
-
+import datetime
+ 
+x = datetime.datetime.now()
+ 
+# Initializing flask app
 app = Flask(__name__)
-
-@app.route('/helloworld')
-def hello_world():
-    response_body = {
-        "name": "AI",
-        "about" :"Hello World! I'm an AI model"
-    }
-
-    return response_body
+ 
+# Route for seeing a data
+@app.route('/data')
+def get_time():
+ 
+    # Returning an api for showing in  reactjs
+    return {
+        'Name':"geek", 
+        "Age":"22",
+        "Date":x, 
+        "programming":"python"
+        }
+ 
+     
+# Running app
+if __name__ == '__main__':
+    app.run(debug=True)
