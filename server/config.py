45b6b431efbb7b67c6a22c877c7cf75b104ae93b
vy <vy@Ubuntu22.04.myguest.virtualbox.org>
2023-11-20 18:58:05 +0200

    add database and redis to env

--- server/config.py
+++ server/config.py
@@ -8,14 +8,13 @@ class AppConfig(object):
     SECRET_KEY = os.environ["SECRET_KEY"]
     SQLALCHEMY_TRACK_MODIFICATION = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://studiodb_x25b_user:l9UFkSHJ5d6In3GTAJi5a1g4hOPHlTav@dpg-cl7t39f6e7vc73a0mvtg-a/studiodb_x25b"
-    # My Local db
+    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
     # SQLALCHEMY_DATABASE_URI = "postgresql://vy:vyadmin12345@localhost/studiodb"
 
     # Set Flask Session
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://red-cl7tg8v6e7vc73a0r0t0:6379")
+    SESSION_REDIS = redis.from_url(os.environ["SESSION_REDIS"])
     #My local redis
     #SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
